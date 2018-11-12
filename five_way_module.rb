b4a7121bda48d7d440d02035136ab8cb7e098d6e
Youssef FKHAR <y.fkhar@yahoo.com>
2018-11-12 12:36:04 +0100

    add comments and singleton class method to module A

--- five_way_module.rb
+++ five_way_module.rb
@@ -7,27 +7,47 @@
 # while instance methods may not
 # source: https://ruby-doc.org/core-2.5.3/Module.html
 module A
+  VERSION = '0.0.1'.freeze
+  # instance method => Class.new.mod_a
   def mod_a
-    'method called from module A'
+    'instance method called from module A'
+  end
+
+  # singleton method => A.k_mod
+  def self.version
+    VERSION
   end
 end
 
-# the module A is mixid in the class K
+# the module A get mixid in the class K, using the 'include' method
+# Notice: mod_a method isn't defined in the class of which the object is an instance
 # the module_a can be called by K instances
+
 class K
   include A
 end
 
-instance = K.new
-p instance.mod_a
-
 # checkin the K class ancestors
 p K.ancestors
 
+# Check
+# class
+p K.instance_methods.include? :mod_a
+# module
+p A.instance_methods.include? :mod_a
+
+# Now K instances can call instance methods of module A
+k_instance = K.new
+p k_instance.mod_a
+
+# == A::VERSION
+p A.version
 
 
 
-# Second Way: Establishing namespaces with modules and nesting
+# Second Way: Nesting modules
+# modules encourage modular design: program design that breaks large components
+# into smaller ones and lets you mix and match object behaviors.
 
 module C
   CONST = "Module #{self}"
@@ -35,12 +55,13 @@ module C
   module D
     CONST = "Module #{self}"
 
-    def self.mod_d
+    def mod_d
       'method called from module D'
     end
+
   end
 end
 
-p C::D.mod_d
+#p C::D.mod_d   # ~> NoMethodError: undefined method `mod_d' for C::D:Module
 p C::CONST
 p C::D::CONST
