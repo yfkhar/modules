8fba1f27be14b20cbfc4ced2adec7b0c513d541a
Youssef FKHAR <y.fkhar@yahoo.com>
2018-11-12 17:31:12 +0100

    add comment and remove p method in the mixin_require.rb

--- mixin_require.rb
+++ mixin_require.rb
@@ -1,4 +1,6 @@
-p $LOAD_PATH.unshift(File.expand_path(__dir__))
+# add the current directory to default load path
+# instead of using require './lib/external_module.rb'
+$LOAD_PATH.unshift(File.expand_path(__dir__))
 require 'lib/external_module'
 
 class Foo
