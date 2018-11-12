74138f0f65126b43417eb350277281e232e95c16
Youssef FKHAR <y.fkhar@yahoo.com>
2018-11-12 17:33:29 +0100

    notice: $: as $LOAD_PATH

--- mixin_require.rb
+++ mixin_require.rb
@@ -1,6 +1,6 @@
 # add the current directory to default load path
 # instead of using require './lib/external_module.rb'
-$LOAD_PATH.unshift(File.expand_path(__dir__))
+$LOAD_PATH.unshift(File.expand_path(__dir__)) # $: == $LOAD_PATH
 require 'lib/external_module'
 
 class Foo
